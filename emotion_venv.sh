5ac1cdd24025c22c775b1358cfd7f0d186a049c1
Ruta.S <54916671+Rutatu@users.noreply.github.com>
2021-05-20 13:27:59 +0200

    Add files via upload

---
+++ emotion_venv.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+VENVNAME=emotion
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+# install ipython kernel, so we can use JHub
+pip install ipython
+pip install jupyter
+python -m ipykernel install --user --name=$VENVNAME
+
+
+# if there is a requirement .txt file
+test -f requirements.txt && pip install -r requirements.txt
+
+# install vggface model
+pip install git+https://github.com/rcmalli/keras-vggface.git
+pip install Keras-Applications
+
+
+
+deactivate
+echo "build $VENVNAME"
